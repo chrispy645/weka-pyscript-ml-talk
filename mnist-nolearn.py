b5e6a4857f142fd8628f92d290d9aa54fec0d141
Christopher Beckham <chrispy645@gmail.com>
2015-08-29 21:35:33 +1200

    Stuffffff

--- mnist-nolearn.py
+++ mnist-nolearn.py
@@ -4,9 +4,6 @@ from lasagne.updates import nesterov_momentum
 from lasagne.nonlinearities import softmax
 
 from nolearn.lasagne import NeuralNet
-from nolearn.lasagne import BatchIterator
-from nolearn.lasagne import PrintLayerInfo
-from nolearn.lasagne import PrintLog
 
 import matplotlib.pyplot as plt
 
@@ -15,9 +12,6 @@ from nolearn.lasagne.visualize import plot_conv_weights
 import gzip
 import cPickle as pickle
 import os
-
-from skimage import io
-from skimage import img_as_float
 
 import numpy as np
 
@@ -28,65 +22,90 @@ import re
 import nolearn_vis
 import draw_net
 
+def get_net(num_epochs):
+    net = NeuralNet(
+        layers = [
+            ('l_in', layers.InputLayer),
+            ('l_conv1', layers.Conv2DLayer),
+            ('l_pool1', layers.MaxPool2DLayer),
+            ('l_conv2', layers.Conv2DLayer),
+            ('l_pool2', layers.MaxPool2DLayer),
+            ('l_hidden', layers.DenseLayer),
+            ('l_out', layers.DenseLayer)
+        ],
 
-net = NeuralNet(
-    layers = [
-        ('l_in', layers.InputLayer),
-        ('l_conv1', layers.Conv2DLayer),
-        ('l_pool1', layers.MaxPool2DLayer),
-        ('l_conv2', layers.Conv2DLayer),
-        ('l_pool2', layers.MaxPool2DLayer),
-        ('l_hidden', layers.DenseLayer),
-        ('l_out', layers.DenseLayer)
-    ],
+        l_in_shape = (None, 1, 28, 28),
+        l_conv1_filter_size=(5,5), l_conv1_num_filters=20/2,
+        l_pool1_pool_size=(2,2),
+        l_conv2_filter_size=(5,5), l_conv2_num_filters=50/2,
+        l_pool2_pool_size=(2,2),
+        l_hidden_num_units=500/2, l_hidden_nonlinearity=lasagne.nonlinearities.rectify, l_hidden_W=lasagne.init.GlorotUniform(),
+        l_out_num_units=10, l_out_nonlinearity=lasagne.nonlinearities.softmax, l_out_W=lasagne.init.GlorotUniform(),
 
-    l_in_shape = (None, 1, 28, 28),
-    l_conv1_filter_size=(5,5), l_conv1_num_filters=20/2,
-    l_pool1_pool_size=(2,2),
-    l_conv2_filter_size=(5,5), l_conv2_num_filters=50/2,
-    l_pool2_pool_size=(2,2),
-    l_hidden_num_units=500/2, l_hidden_nonlinearity=lasagne.nonlinearities.rectify, l_hidden_W=lasagne.init.GlorotUniform(),
-    l_out_num_units=10, l_out_nonlinearity=lasagne.nonlinearities.softmax, l_out_W=lasagne.init.GlorotUniform(),
+        update=nesterov_momentum,
+        update_learning_rate=0.001,
+        update_momentum=0.9,
+        verbose=1,
+        max_epochs=num_epochs,
+    )
+    return net
 
-    update=nesterov_momentum,
-    update_learning_rate=0.001,
-    update_momentum=0.9,
-    verbose=1,
-    max_epochs=10
-)
+def train(args):
+    X_train = args["X_train"]
+    X_train = X_train.reshape( (X_train.shape[0], 1, 28, 28) )
+    y_train = np.asarray( args["y_train"].flatten(), dtype="int32" )
+    num_epochs = 1 if "num_epochs" not in args else args["num_epochs"]
+    net = get_net(num_epochs)
+    net.fit(X_train, y_train)
+    if "vis" in args and args["vis"] == True:
+        nolearn_vis.plot_conv_activity( net.layers_[1], X_train[0:1] )
+    return net.get_all_params_values()
 
-"""
-X_train = np.asarray([ nolearn_vis.load_image("1002_c2.gif") ], dtype="float32")
-y_train = np.asarray([2], dtype="int32")
-net.initialize()
+def describe(args, model):
+    return "my conv net"
 
-print net.layers_
+def test(args, model):
+    X_test = args["X_test"]
+    X_test = X_test.reshape( (X_test.shape[0], 1, 28, 28) )
+    num_epochs = 1 if "num_epochs" not in args else args["num_epochs"]
+    net = get_net(num_epochs)
+    net.initialize()
+    net.load_params_from(model)
+    return net.predict_proba(X_test).tolist()
 
-nolearn_vis.plot_network(net.layers_, X_train)
+if __name__ == '__main__':
 
-#net.fit(X_train, y_train)
-"""
+    #net.initialize_layers()
 
-net.initialize_layers()
+    f = gzip.open("mnist.pkl.gz")
+    args = pickle.load(f)
+    f.close()
 
-f = gzip.open("mnist.pkl.gz")
-args = pickle.load(f)
-f.close()
+    #args["vis"] = True
+    args["num_epochs"] = 20
+    model_params = train(args)
 
-X_train = args["X_train"]
-X_train = X_train.reshape( (10000, 1, 28, 28) )
-y_train = np.asarray( args["y_train"].flatten(), dtype="int32" )
+    args["X_test"] = args["X_train"]
 
-print dir(net.layers_)
+    predictions = test(args, model_params)
 
-layers = []
-for elem in net.layers_:
-    layers.append(net.layers_[elem])
+    predicted_labels = []
+    actual_labels = args["y_train"].flatten().tolist()
+    for pred in predictions:
+        predicted_labels.append( np.argmax(pred) )
 
-print draw_net.draw_to_file(layers, "/tmp/graph.png")
+    print "Accuracy is: %f" % (float( np.sum( np.equal(actual_labels, predicted_labels) ) ) / len(actual_labels))
 
-net.fit(X_train, y_train)
+    """
+    layers = []
+    for elem in net.layers_:
+        layers.append(net.layers_[elem])
 
-#nolearn_vis.plot_loss(net)
+    #print draw_net.draw_to_file(layers, "/tmp/graph.png")
 
-nolearn_vis.plot_network(net.layers_, X_train[0:1])
+    #net.fit(X_train, y_train)
+
+    #nolearn_vis.plot_loss(net)
+
+    nolearn_vis.plot_network(net.layers_, X_train[0:1])
+    """
